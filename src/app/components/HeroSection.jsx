0678366d6d48ad3ee24c778e7e3b891b7dc02077
MartinCout <60389576+martincout@users.noreply.github.com>
2024-08-11 16:06:57 -0300

    Removed Achievemnts section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -14,7 +14,7 @@ const HeroSection = () => {
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.5 }}
-        className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        className="col-span-8 place-self-center text-center sm:text-left justify-self-center"
       >
         <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
@@ -24,17 +24,11 @@ const HeroSection = () => {
           <TypeAnimation
             sequence={[
               "Martin",
-              4000,
-              "Web Developer",
-              1000,
-              "Backend Developer",
-              1000,
-              "Mobile Developer",
-              1000,
+              5000
             ]}
             wrapper="span"
-            speed={50}
-            repeat={Infinity}
+            delay={500}
+            speed={5}
           />
         </h1>
         <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
